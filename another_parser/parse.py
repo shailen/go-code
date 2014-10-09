511571c17299508b3056f7d76730c166ebb00505
Takeshi Hagikura <thagikura@google.com>
2014-10-09 12:49:50 -0600

    Fixes the issue IMPORT doesn' work with relative path

--- another_parser/parse.py
+++ another_parser/parse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import logging
 import re
 import urllib2
@@ -54,11 +55,19 @@ def replace_import(file_name, output_file_name):
                 for line in read_file.readlines():
                     import_in_line = re.search(REGEX_IMPORT, line)
                     if import_in_line:
-                        import_function = ImportFunction(import_in_line.group(1), import_in_line.group(2))
+                        if _is_relative_path(import_in_line.group(1)):
+                            file_path_to_extract = os.path.dirname(file_name) + '/' + import_in_line.group(1)
+                        else:
+                            file_path_to_extract = import_in_line.group(1)
+                        import_function = ImportFunction(file_path_to_extract, import_in_line.group(2))
                         extracted = import_function.extract_tagged_lines()
                         write_file.write(extracted)
                     else:
                         write_file.write(line)
+
+
+def _is_relative_path(path_name):
+    return not (path_name.startswith('/') or path_name.startswith('https://') or path_name.startswith('http://'))
 
 
 def _includes_import(file_name):
@@ -68,6 +77,7 @@ def _includes_import(file_name):
 
 
 if __name__ == '__main__':
-    print 'converting', sys.argv[1], 'to', sys.argv[2] 
+    print 'converting', sys.argv[1], 'to', sys.argv[2]
     replace_import(sys.argv[1], sys.argv[2])
+
 
